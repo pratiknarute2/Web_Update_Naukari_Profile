08820fc2d72cbb7ca950cb7f35f9fb4cd0e84866
prajwalnarute <prajwalnarute100@gmail.com>
2026-02-01 19:11:42 +0530

    narendra profile added

--- Tests/UpdateProfile.test.js
+++ Tests/UpdateProfile.test.js
@@ -2,22 +2,26 @@ const { test, expect } = require('@playwright/test');
 const Job = require("../Pages/Job.js");
 
 test.describe.parallel('Update Profile', () => {
-    test('Update Naukri Profile of Prajwal for Pune Location', async ({ page }) => {
-        const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute2002@gmail.com', 'Prajwal@123', 'Prajwal_Narute.pdf', 'Node.js')
-    });
-    test('Update Naukri Profile of Prajwal for Banglore Location', async ({ page }) => {
-        const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute100@gmail.com', 'Prajwal@123', 'Prajwal_Narute2.pdf', 'Node.js')
-    });
-    test('Update Naukri Profile of Prajwal for Hyderabad Location', async ({ page }) => {
-        const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute12@gmail.com', 'Prajwal@123', 'Prajwal_3.pdf', 'Node.js')
-    });
-    test('Update Naukri Profile of Pratik for Mumbai Location', async ({ page }) => {
-        const job = new Job(page)
-        await job.updateNaukaryProfile('pratiknarute2@gmail.com', 'Pratik@123', 'Pratik_Resume.pdf', 'Python')
-    });
+    // test('Update Naukri Profile of Prajwal for Pune Location', async ({ page }) => {
+    //     const job = new Job(page)
+    //     await job.updateNaukaryProfile('prajwalnarute2002@gmail.com', 'Prajwal@123', 'Prajwal_Narute.pdf', 'Node.js')
+    // });
+    // test('Update Naukri Profile of Prajwal for Banglore Location', async ({ page }) => {
+    //     const job = new Job(page)
+    //     await job.updateNaukaryProfile('prajwalnarute100@gmail.com', 'Prajwal@123', 'Prajwal_Narute2.pdf', 'Node.js')
+    // });
+    // test('Update Naukri Profile of Prajwal for Hyderabad Location', async ({ page }) => {
+    //     const job = new Job(page)
+    //     await job.updateNaukaryProfile('prajwalnarute12@gmail.com', 'Prajwal@123', 'Prajwal_Resume.pdf', 'Node.js')
+    // });
+    // test('Update Naukri Profile of Pratik for Mumbai Location', async ({ page }) => {
+    //     const job = new Job(page)
+    //     await job.updateNaukaryProfile('pratiknarute2@gmail.com', 'Pratik@123', 'Pratik_Resume.pdf', 'Python')
+    // });
 
+    test('Update Naukri Profile of Narendra for Bhopal Location', async ({ page }) => {
+        const job = new Job(page)
+        await job.updateNaukaryProfile('narendraahirwar0306@gmail.com', 'Narendra.@123', 'Narendra_Resume.pdf', 'Node.js')
+    });
 
 })
