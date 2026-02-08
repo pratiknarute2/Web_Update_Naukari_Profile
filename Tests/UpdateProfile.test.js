f264ce888e4f8a13ad49fe4c0a68ceef313b78ea
prajwalnarute <prajwalnarute100@gmail.com>
2026-02-08 22:19:42 +0530

    prajwal's profiles resume updated

--- Tests/UpdateProfile.test.js
+++ Tests/UpdateProfile.test.js
@@ -4,15 +4,15 @@ const Job = require("../Pages/Job.js");
 test.describe.parallel('Update Profile', () => {
     test('Update Naukri Profile of Prajwal for Pune Location', async ({ page }) => {
         const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute2002@gmail.com', 'Prajwal@123', 'Prajwal_Narute.pdf', 'Node.js')
+        await job.updateNaukaryProfile('prajwalnarute2002@gmail.com', 'Prajwal@123', 'Prajwal_Resume_Pune.pdf', 'Scalability')
     });
     test('Update Naukri Profile of Prajwal for Banglore Location', async ({ page }) => {
         const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute100@gmail.com', 'Prajwal@123', 'Prajwal_Narute2.pdf', 'Node.js')
+        await job.updateNaukaryProfile('prajwalnarute100@gmail.com', 'Prajwal@123', 'Prajwal_Resume_Bengaluru.pdf', 'Scalability')
     });
     test('Update Naukri Profile of Prajwal for Hyderabad Location', async ({ page }) => {
         const job = new Job(page)
-        await job.updateNaukaryProfile('prajwalnarute12@gmail.com', 'Prajwal@123', 'Prajwal_Resume.pdf', 'Node.js')
+        await job.updateNaukaryProfile('prajwalnarute12@gmail.com', 'Prajwal@123', 'Prajwal_Resume_Hyderabad.pdf', 'Scalability')
     });
     test('Update Naukri Profile of Pratik for Mumbai Location', async ({ page }) => {
         const job = new Job(page)
